4b936a121e204cc910b7d02bff1cfb23e91c1bc7
In4matik <in4matik@hotmail.fr>
2013-09-17 09:26:05 +0200

    Fixed error in ship update

--- includes/player_load.php
+++ includes/player_load.php
@@ -1,17 +1,20 @@
 <?php
 $array_fleets = Fleet::getAll('', '', $User->getId());
 $array_moves = Move::getAll('', '', $User->getId(), 'flying');
-$array_ships = Ship::getAll('', '', $User->getId());
 
 if (!empty($array_moves)) {
-    foreach ($array_moves as $Move)
+    foreach ($array_moves as $i => $Move)
     {
         if ($Move->countRemainingTime() < 0) {
             $Move->land();
+            unset($array_moves[$i]);
         }
     }
 }
 
+
+
+$array_ships = Ship::getAll('', '', $User->getId());
 if (!empty($array_ships)) {
     foreach ($array_ships as $Ship)
     {
