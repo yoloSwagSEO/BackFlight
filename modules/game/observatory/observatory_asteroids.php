9a0799505925bb7fae794c03e03dca4c2b751b91
In4matik <in4matik@hotmail.fr>
2013-09-19 09:35:50 +0200

    Add "go to known destination"

--- modules/game/observatory/observatory_asteroids.php
+++ modules/game/observatory/observatory_asteroids.php
@@ -2,6 +2,7 @@
             <p>Que souhaitez-vous faire ?</p>
             <a href="#" data-dropdown="land" class="button">Déployer le vaisseau sur un astéroïde</a>
             <a href="#" data-dropdown="continue" class="button">Poursuivre le vol</a>
+            <a href="#" data-dropdown="reach" class="button">Atteindre une position connue</a>
         </div>
     </div>
 </div>
@@ -9,7 +10,6 @@
   <li data-tooltip class="has-tip" title="Le vaisseau tentera de se poser sur l'astéroïde le plus proche.<br />Arrivée : 30s<br />"><a href="fly/asteroid/fast" >Déploiement rapide</a></li>
   <li data-tooltip class="has-tip" title="Après une phase d'observation, le vaisseau tentera de se poser sur l'astéroïde le plus adapté. <br />Arrivée : 3m"><a href="fly/asteroid">Observation et déploiement</a></li>
 </ul>
-<ul id="continue" class="f-dropdown" data-dropdown-content>
-  <li data-tooltip class="has-tip" title="Déplacement ultra-rapide vers une autre zone de l'espace.<br />Prochain bond : 30s<br />Energie : 20"><a href="fly/space/fast">Bond hyperespace</a></li>
-  <li data-tooltip class="has-tip" title="Déplacement vers une autre zone de l'espace. <br />Arrivée : 6m<br />Energie : 5"><a href="fly/space">Vol hyperespace</a></li>
-</ul>
+<?php
+include_once 'modules/game/observatory/observatory_continue.php';
+?>
--- modules/game/observatory/observatory_space.php
+++ modules/game/observatory/observatory_space.php
@@ -2,6 +2,7 @@
             <p>Que souhaitez-vous faire ?</p>
             <a href="#" data-dropdown="search" class="button">Sonder l'espace</a>
             <a href="#" data-dropdown="continue" class="button">Poursuivre le vol</a>
+            <a href="#" data-dropdown="reach" class="button">Atteindre une position connue</a>
         </div>
     </div>
 </div>
@@ -9,7 +10,6 @@
     <li data-tooltip class="has-tip" title="Recherche rapide d'objet ou de ressources dans l'espace.<br />Fin : 30s<br />Proba : 20%"><a href="search/fast" >Sonder depuis le vaisseau</a></li>
     <li data-tooltip class="has-tip" title="Les deux sondes du vaisseau partent à la recherches d'objet ou de ressoources dans l'espace. <br />Retour : 4m15<br />Proba : 100%"><a href="fly/asteroid">Larguer les sondes</a></li>
 </ul>
-<ul id="continue" class="f-dropdown" data-dropdown-content>
-  <li data-tooltip class="has-tip" title="Déplacement ultra-rapide vers une autre zone de l'espace.<br />Prochain bond : 30s<br />Energie : 20"><a href="fly/space/fast">Bond hyperespace</a></li>
-  <li data-tooltip class="has-tip" title="Déplacement vers une autre zone de l'espace. <br />Arrivée : 6m<br />Energie : 5"><a href="fly/space">Vol hyperespace</a></li>
-</ul>
+<?php
+include_once 'modules/game/observatory/observatory_continue.php';
+?>
