58cb444b84159f44308629b8b661370fc652936e
In4matik <in4matik@hotmail.fr>
2013-10-01 18:04:51 +0200

    Global SQL update

--- sql_tables.sql
+++ sql_tables.sql
@@ -1,12 +1,38 @@
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `actions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(12) NOT NULL,
-  `password` varchar(70) NOT NULL,
-  `mail` text NOT NULL,
-  `rank` varchar(10) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `pseudo` (`pseudo`)
+  `user` int(11) NOT NULL,
+  `type` varchar(25) NOT NULL,
+  `from` int(11) NOT NULL,
+  `to` int(11) NOT NULL,
+  `start` int(11) DEFAULT NULL,
+  `duration` int(11) NOT NULL,
+  `end` int(11) DEFAULT NULL,
+  `state` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `fleets` (
+  `moveId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `shipId` int(11) NOT NULL,
+  KEY `moveId` (`moveId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `models` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) NOT NULL,
+  `user` int(11) DEFAULT NULL,
+  `category` varchar(25) NOT NULL,
+  `type` varchar(25) DEFAULT NULL,
+  `modulesMax` int(11) NOT NULL,
+  `loadMax` int(11) NOT NULL,
+  `energyMax` int(11) NOT NULL,
+  `energyGain` int(11) NOT NULL,
+  `fuelMax` int(11) NOT NULL,
+  `powerMax` int(11) NOT NULL,
+  `speed` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `positions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,81 +42,7 @@ CREATE TABLE IF NOT EXISTS `positions` (
   `category` varchar(10) NOT NULL,
   `type` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-CREATE TABLE IF NOT EXISTS `ships` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `playerId` int(11) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  `positionId` int(11) NOT NULL,
-  `state` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-ALTER TABLE  `positions` CHANGE  `zone`  `zone` INT( 11 ) NULL ;
-ALTER TABLE  `ships` CHANGE  `playerId`  `userId` INT( 11 ) NOT NULL ;
-ALTER TABLE  `ships` ADD  `model` INT NOT NULL AFTER  `type` ;
-
-CREATE TABLE IF NOT EXISTS `models` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(25) NOT NULL,
-  `user` int(11) DEFAULT NULL,
-  `category` varchar(25) NOT NULL,
-  `type` varchar(25) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `moves` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `user` INT(11) NOT NULL,
-    `type` varchar(25) NOT NULL,
-    `from` INT(11) NOT NULL,
-    `to` INT(11) NOT NULL,
-    `start` INT(11) NULL,
-    `duration` INT(11) NOT NULL,
-    `end` INT(11) NULL,
-    `state` varchar(25) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `fleets` (
-  `moveId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `shipId` int(11) NOT NULL,
-  KEY `moveId` (`moveId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE  `models` ADD  `loadMax` INT NOT NULL ,
-ADD  `energyMax` INT NOT NULL ,
-ADD  `fuelMax` INT NOT NULL ,
-ADD  `powerMax` INT NOT NULL ,
-ADD  `speed` INT NOT NULL ;
-
-ALTER TABLE  `ships` ADD  `load` INT NOT NULL AFTER  `positionId` ,
-ADD  `energy` INT NOT NULL AFTER  `load` ,
-ADD  `fuel` INT NOT NULL AFTER  `energy` ,
-ADD  `power` INT NOT NULL AFTER  `fuel` ;
-
-ALTER TABLE  `models` ADD  `energyGain` INT NOT NULL AFTER  `energyMax` ;
-ALTER TABLE  `ships` ADD  `lastUpdate` INT NOT NULL AFTER  `power` ;
-
-CREATE TABLE IF NOT EXISTS `users_positions` (
-  `userId` int(11) NOT NULL,
-  `positionId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `ressources` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `userId` INT(11) NOT NULL,
-    `into` varchar(10) NOT NULL,
-    `intoId` INT(11) NOT NULL,
-    `type` varchar(10) NOT NULL,
-    `quantity` INT(11) NOT NULL,
-    `quality` INT(11) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE  `ships` DROP  `fuel` ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `positions_searches` (
   `positionId` int(11) NOT NULL,
@@ -99,9 +51,42 @@ CREATE TABLE IF NOT EXISTS `positions_searches` (
   `result` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE  `ships` CHANGE  `load`  `load` FLOAT NOT NULL ,
-CHANGE  `energy`  `energy` FLOAT NOT NULL ,
-CHANGE  `power`  `power` FLOAT NOT NULL ;
+CREATE TABLE IF NOT EXISTS `ressources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `into` varchar(10) NOT NULL,
+  `intoId` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `quality` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-RENAME TABLE  `backflight`.`moves` TO  `backflight`.`actions` ;
+CREATE TABLE IF NOT EXISTS `ships` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `model` int(11) NOT NULL,
+  `positionId` int(11) NOT NULL,
+  `load` float NOT NULL,
+  `energy` float NOT NULL,
+  `power` float NOT NULL,
+  `lastUpdate` int(11) NOT NULL,
+  `state` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(12) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  `mail` text NOT NULL,
+  `rank` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pseudo` (`pseudo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `users_positions` (
+  `userId` int(11) NOT NULL,
+  `positionId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
