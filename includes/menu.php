30ceb763f0b9a5485c290740fec0b8c2028b2a39
In4matik <in4matik@hotmail.fr>
2013-10-10 07:39:44 +0200

    Move all menus to a single file

---
+++ includes/menu.php
@@ -0,0 +1,6 @@
+<ul class="side-nav">
+    <li><a href="overview">Ma position</a></li>
+    <li><a href="observatory">Observatoire</a></li>
+    <li><a href="quests">Quêtes</a></li>
+    <li><a href="ship">Vaisseau</a></li>
+</ul>
