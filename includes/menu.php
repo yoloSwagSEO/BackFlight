eedbd2cf768f6665378d8f193072dcd6c9c2dac6
In4matik <in4matik@hotmail.fr>
2013-10-15 10:01:58 +0200

    Add number of unread conversations in menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,24 @@
+<?php
+$nb_conversation_unread = 0;
+
+foreach ($array_conversations as $Conversation)
+{
+    if (!$Conversation->isRead()) {
+        $nb_conversation_unread++;
+    }
+}
+
+$menu_add = '';
+if ($nb_conversation_unread) {
+    $menu_add = ' ('.$nb_conversation_unread.')';
+}
+
+?>
+
+
 <ul class="side-nav">
     <li><a href="overview">Ma position</a></li>
-    <li><a href="messages">Messages</a></li>
+    <li><a href="messages">Messages<?php echo $menu_add?></a></li>
     <li><a href="observatory">Observatoire</a></li>
     <li><a href="quests">Quêtes</a></li>
     <li><a href="ship">Vaisseau</a></li>
