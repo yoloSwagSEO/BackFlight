c91243063b8b7c0df1d58d50a0720da43a6a8652
In4matik <in4matik@hotmail.fr>
2013-10-22 12:26:55 +0200

    Fix weapon displaying when qty=0

--- modules/game/weapons/weapons_check_post.php
+++ modules/game/weapons/weapons_check_post.php
@@ -10,30 +10,32 @@ $array_weapons_use = array();
 $type = '';
 foreach ($array_weapons_post as $objectId => $quantity)
 {
-    if (empty($array_weapons[$objectId])) {
-        exit('Unknown object');
-    }
-
-    $ObjectWeapon = $array_weapons[$objectId];
-
-    if (empty($type)) {
-        $type = $ObjectWeapon->getObjectType();
-    } else {
-        if ($type != $ObjectWeapon->getObjectType()) {
-            exit('Can\'t use torpedoes and mines together');
+    if ($quantity > 0) {
+        if (empty($array_weapons[$objectId])) {
+            exit('Unknown object');
         }
+
+        $ObjectWeapon = $array_weapons[$objectId];
+
+        if (empty($type)) {
+            $type = $ObjectWeapon->getObjectType();
+        } else {
+            if ($type != $ObjectWeapon->getObjectType()) {
+                exit('Can\'t use torpedoes and mines together');
+            }
+        }
+
+
+        // Check for availability
+        if (!$MasterShipPlayer->hasObjectAvailable('weapon', $ObjectWeapon->getId())) {
+            exit('No object on board');
+        }
+
+        $quantity = $MasterShipPlayer->getObject('weapons', $ObjectWeapon->getId());
+        if ($quantity < $array_weapons_post[$ObjectWeapon->getId()]) {
+            exit('Not so much objects onboard');
+        }
+
+        $array_weapons_use[$ObjectWeapon->getId()] = $ObjectWeapon;
     }
-
-
-    // Check for availability
-    if (!$MasterShipPlayer->hasObjectAvailable('weapon', $ObjectWeapon->getId())) {
-        exit('No object on board');
-    }
-
-    $quantity = $MasterShipPlayer->getObject('weapons', $ObjectWeapon->getId());
-    if (!$quantity >= $array_weapons_post[$ObjectWeapon->getId()]) {
-        exit('Not so much objects onboard');
-    }
-
-    $array_weapons_use[$ObjectWeapon->getId()] = $ObjectWeapon;
 }
