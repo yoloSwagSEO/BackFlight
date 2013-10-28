a7db41d28b6036db4bdfc46432c89f311db86f05
sarjevane <groot.samuel@gmail.com>
2013-10-28 03:27:19 +0100

    Correct typo

--- modules/game/drop/drop_drop.php
+++ modules/game/drop/drop_drop.php
@@ -6,14 +6,14 @@ $quantity = preg_replace("[^0-9]", "", $_POST['quantity']);
 $type = $_GET['type'];
 $types = array ('fuel', 'techs');
 if (!in_array($type, $types)) {
-	exit('You can only drop techs and fuel !');
+    exit('You can only drop techs and fuel !');
 }
 
 // Define max value
 if ($type == 'techs') {
-	$max = number_format($MasterShipPlayer->getTechs());
+    $max = number_format($MasterShipPlayer->getTechs());
 } else {
-	$max = number_format($MasterShipPlayer->getFuel());
+    $max = number_format($MasterShipPlayer->getFuel());
 }
 
 // If quantity < 0, quantity = 0
@@ -21,12 +21,13 @@ if ($type == 'techs') {
 $quantity = max(min($max,$quantity),0);
 
 if ($type == 'fuel') {
-	$MasterShipPlayer->removeFuel($quantity);
-	$_SESSION['infos']['drop']['fuel'] = $quantity;
+    $MasterShipPlayer->removeFuel($quantity);
+    $_SESSION['infos']['drop']['fuel'] = $quantity;
 } else {
-	$MasterShipPlayer->removeTechs($quantity);
-	$_SESSION['infos']['drop']['techs'] = $quantity;
+    $MasterShipPlayer->removeTechs($quantity);
+    $_SESSION['infos']['drop']['techs'] = $quantity;
 }
+// Drop ressources and save
 $MasterShipPlayer->save();
 
 // Exit.
